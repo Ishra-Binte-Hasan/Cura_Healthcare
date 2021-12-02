5368574a39289eec7d8ce1dade935d4c963424b2
Ishra-Binte-Hasan <ishrahasan12@gmail.com>
2021-12-03 01:30:22 +0600

    commit messahe

--- src/homepage.java
+++ src/homepage.java
@@ -5,19 +5,23 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 public class homepage {
 
-	public static void main(String[] args)  
+	public static void main(String[] args) throws InterruptedException  
 	{
 		
-		System.setProperty("webdriver.chrome.driver", "D:\\Downloads (D drive)\\chromedriver_win32\\chromedriver.exe");
+		System.setProperty("webdriver.chrome.driver", "D:\\\\Downloads (D drive)\\\\chromedriver_win32 (1)\\chromedriver.exe");
         WebDriver driver=new ChromeDriver();
         driver.get("https://katalon-demo-cura.herokuapp.com/");
+        Thread.sleep(2000);
         driver.findElement(By.id("menu-toggle")).click();  
+        Thread.sleep(2000);
         driver.findElement(By.id("btn-make-appointment")).click();
+        Thread.sleep(2000);
         driver.findElement(By.xpath("//a[contains(text(),'info@katalon.com')]")).click();
-        System.out.println(driver.findElement(By.xpath("//strong[contains(text(),'CURA Healthcare Service')]")).getText());
-        System.out.println(driver.findElement(By.tagName("p")).getText());
+        Thread.sleep(2000);
+        //System.out.println(driver.findElement(By.xpath("//strong[contains(text(),'CURA Healthcare Service')]")).getText());
+       // System.out.println(driver.findElement(By.tagName("p")).getText());
         //System.out.println(driver.findElement(By.xpath("//*[text='(678)813-1KMS']")).getText());
-         driver.close();
+        driver.close();
 
 	}
 
--- src/login.java
+++ src/login.java
@@ -6,32 +6,55 @@ import org.openqa.selenium.support.ui.Select;
 
 public class login{
 
-	public static void main(String[] args)  
+	public static void main(String[] args) throws InterruptedException  
 	{
 		
-		System.setProperty("webdriver.chrome.driver", "D:\\Downloads (D drive)\\chromedriver_win32\\chromedriver.exe");
+		System.setProperty("webdriver.chrome.driver", "D:\\Downloads (D drive)\\chromedriver_win32 (1)\\chromedriver.exe");
         WebDriver driver=new ChromeDriver();
         driver.get("https://katalon-demo-cura.herokuapp.com/");
         driver.findElement(By.id("menu-toggle")).click();
+        Thread.sleep(2000);
         driver.findElement(By.xpath("//a[contains(text(),'Login')]")).click();
         driver.findElement(By.id("txt-username")).sendKeys("John Doe");
+        Thread.sleep(2000);
         driver.findElement(By.id("txt-password")).sendKeys("ThisIsNotAPassword");
+        Thread.sleep(2000);
         driver.findElement(By.id("btn-login")).click();
         Select s= new Select(driver.findElement(By.id("combo_facility")));
+        Thread.sleep(5000);
         s.selectByValue("Hongkong CURA Healthcare Center");
+        Thread.sleep(2000);
         driver.findElement(By.id("chk_hospotal_readmission")).click();
+        Thread.sleep(2000);
         driver.findElement(By.id("radio_program_medicaid")).click();
+        Thread.sleep(2000);
         WebElement dateBox = driver.findElement(By.xpath("//input[@id='txt_visit_date']"));
         dateBox.sendKeys("18072021");
         driver.findElement(By.id("txt_comment")).sendKeys("Hello!");
+        Thread.sleep(2000);
         driver.findElement(By.id("btn-book-appointment")).click();
+        Thread.sleep(2000);
         driver.findElement(By.id("menu-toggle")).click();
+        Thread.sleep(2000);
         driver.findElement(By.xpath("//a[contains(text(),'History')]")).click();
+        Thread.sleep(2000);
         driver.findElement(By.xpath("//a[contains(text(),'Go to Homepage')]")).click();
+        Thread.sleep(2000);
+       
         driver.findElement(By.id("menu-toggle")).click();
+        Thread.sleep(2000);
         driver.findElement(By.xpath("//a[contains(text(),'Profile')]")).click();
+        Thread.sleep(2000);  
         driver.findElement(By.id("menu-toggle")).click();
+        Thread.sleep(2000);
         driver.findElement(By.xpath("//a[contains(text(),'Logout')]")).click();
+        Thread.sleep(2000);
+        //driver.findElement(By.id("menu-toggle")).click();  
+       // Thread.sleep(2000);
+        driver.findElement(By.id("btn-make-appointment")).click();
+       Thread.sleep(2000);
+        driver.findElement(By.xpath("//a[contains(text(),'info@katalon.com')]")).click();
+       Thread.sleep(2000);
         driver.close();
  
 }
